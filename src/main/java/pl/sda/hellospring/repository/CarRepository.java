869a49757f662135e7b40eece21ada32e4daa7b5
maniek <mpastuszka@gmail.com>
2020-09-27 09:18:37 +0200

    [change] - custom repository method implemented

--- src/main/java/pl/sda/hellospring/repository/CarRepository.java
+++ src/main/java/pl/sda/hellospring/repository/CarRepository.java
@@ -3,5 +3,9 @@ package pl.sda.hellospring.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.sda.hellospring.domain.Car;
 
+import java.util.Optional;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    Optional<Car> findByBrandAndColor(String brand, String color);
 }
