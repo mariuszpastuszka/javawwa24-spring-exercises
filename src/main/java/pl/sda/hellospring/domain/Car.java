2a1548c563c5e778f7b39ffb02fa64a4ad2850b0
maniek <mpastuszka@gmail.com>
2020-09-27 10:47:40 +0200

    [add] - service implemented

--- src/main/java/pl/sda/hellospring/domain/Car.java
+++ src/main/java/pl/sda/hellospring/domain/Car.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Data
 @Entity
@@ -18,7 +20,14 @@ public class Car {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "marka", nullable = false)
     private String brand;
+
+    @Column(name = "model")
     private String model;
+
+//    @Transient - don't create column from field
+    @Column(name = "kolor")
     private String color;
 }
